790b96ce404fd99597f02af749a76758f2bb6b09
mengeroshi <dpllgr@hotmail.com>
2020-08-23 15:24:11 -0500

    dropdown event button done

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+/*******Feather Icons********** */
+feather.replace()
+/****************************** */
+
+// MENU BUTTON--MOBILE//
 const menuButton = document.querySelector('.navbar__icon--open');
 const dropdownList = document.querySelector('.navbar__list');
 const mobileBackdrop = document.querySelector('.navbar__mobile-backdrop');
@@ -23,3 +28,24 @@ closeButton.addEventListener('click', ()=>{
     mobileBackdrop.classList.add('undisplayed--mobile');
     dropdownList.classList.add('undisplayed--mobile');
 })
+
+
+
+// MENU BUTTON--MOBILE//
+
+const navbarItems = document.querySelectorAll('.navbar__item');
+
+navbarItems.forEach(item => item.addEventListener('click', (event)=>{
+
+    if(item == navbarItems[0]) return;
+    event.preventDefault();
+
+    let dropdown = item.lastElementChild.classList;
+
+    if(dropdown.contains('displayed--mobile')){
+        dropdown.remove('displayed--mobile');
+
+    }else{
+        dropdown.add('displayed--mobile');
+    }
+}))
